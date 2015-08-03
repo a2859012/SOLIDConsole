297395fe0128856742193ade281caf747d0f4d49
a2859012 <a2859012@hotmail.com>
2015-08-04 00:52:00 +0800

    https://github.com/SOLIDConsole/SOLIDConsole.git

---
+++ SOLIDConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using SOLIDConsole.SingletonPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SOLIDConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton test = Singleton.Instance;
+            test.DoSometing();
+            Singleton.SayHi();
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SOLIDConsole/SingletonPattern/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SOLIDConsole.SingletonPattern
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> LazyInstance = new Lazy<Singleton>(() => new Singleton());
+
+        private Singleton() { }
+
+        public static Singleton Instance { get { return LazyInstance.Value; } }
+
+        public static void SayHi()
+        {
+            Console.WriteLine("Hi All ~~");
+        }
+
+        public void DoSometing()
+        {
+            Console.WriteLine("Do Someting ....");
+        }
+    }
+}
